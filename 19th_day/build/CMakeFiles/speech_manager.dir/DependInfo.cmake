9cbb6934681d504f7e0aa6d0d224f1d506bcc4be
xxx <1250855168@qq.com>
2022-10-13 15:22:27 +0800

    updata

---
+++ 19th_day/build/CMakeFiles/speech_manager.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/xxx/Me/Cpp_study/19th_day/main.cpp" "/home/xxx/Me/Cpp_study/19th_day/build/CMakeFiles/speech_manager.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ 19th_day/build/CMakeFiles/speech_manager.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/speech_manager.dir/main.cpp.o"
+  "speech_manager"
+  "speech_manager.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/speech_manager.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
